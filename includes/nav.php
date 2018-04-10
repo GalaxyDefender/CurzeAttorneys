ee8c47e0a4529629e90b68ef2809a196912d0377
Quentin <qgerard.gerard@gmail.com>
2018-04-10 20:20:46 +0200

    nav include

---
+++ includes/nav.php
@@ -0,0 +1,28 @@
+<img src="img/Curze Logo.png" style="margin-top:30px;" class="img-responsive" alt="">
+    <div class="col-md-offset-6">
+        <h4 class="call-to-action">Call now <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>  084 555 654</h4> 
+    </div>
+
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Attorneys</a></li>
+            <li><a href="#">Construction Law</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div><!-- /.navbar-collapse -->
+</nav>
--- index.php
+++ index.php
@@ -11,34 +11,7 @@
 </head>
 <body>
     <div class="container">
-    <img src="img/Curze Logo.png" style="margin-top:30px;" class="img-responsive" alt="">
-            <div class="col-md-offset-6">
-                <h4 class="call-to-action">Call now <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>  084 555 654</h4> 
-            </div>
-        
-        <nav class="navbar navbar-default" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-        
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Attorneys</a></li>
-                    <li><a href="#">Construction Law</a></li>
-                    <li><a href="#">Careers</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
+    <?php include 'includes/nav.php'?>
 
         <h1 class="heading"><u>Ranked #1</u> <br> Construction Law Firm <br>In South Africa</h1>
         <div class="row" style="margin-top:80px;">
@@ -52,7 +25,8 @@
                 <p>- Another Happy Client</p>
             </div>
         </div>
-    </div>
+
+    </div> <!-- container -->
   
 </body>
 </html>
